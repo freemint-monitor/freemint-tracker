1d21ca99697ea5246b0736a65c9df6b8f688330a
TimGrey998 <timgrey998@gmail.com>
2022-04-24 19:08:08 +0800

    update multiple files

--- utils/play_alarm.js
+++ utils/play_alarm.js
@@ -2,6 +2,6 @@ import Player from 'play-sound'
 
 export const playSound = async () => {
   const player = Player({})
-  let audio = player.play('./alarm/qingtian.mp3') 
+  let audio = player.play('./alarm/alarm.mp3') 
   // audio.kill()
 }
