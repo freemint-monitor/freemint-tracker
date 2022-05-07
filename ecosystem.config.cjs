77b205fcc326797983fb6f99873d7b1a1def6387
TimGrey998 <timgrey998@gmail.com>
2022-05-07 22:47:44 +0800

    edit eco

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,64 +2,69 @@ module.exports = {
   apps: [
     {
       name: "app1",
-      script: "node listen 0xb3ad74fe581e94e491b1ed8bfccb87e53e608870",
+      script: "node listen 0x80e108cffa85f7f4c76bbf0a7c2adcbe53e38f0a",
       error_file: "logs/err.log",
       out_file: "logs/out.log",
     },
     {
       name: "app2",
-      script: "node listen 0x0b01f1310e7224dafed24c3b62d53cec37d9faf8 ",
+      script: "node listen 0xb3ad74fe581e94e491b1ed8bfccb87e53e608870",
       error_file: "logs/err.log",
       out_file: "logs/out.log",
     },
     {
       name: "app3",
-      script: "node listen 0x809e82136239a5f26596bd6ce9192207bdffa04c",
+      script: "node listen 0x6eef09b526d883f98762a7005fabd2c800dfca44",
       error_file: "logs/err.log",
       out_file: "logs/out.log",
     },
     {
       name: "app4",
-      script: "node listen 0x405ce2d22faff3a0420568c84cc660548070ce52",
+      script: "node listen 0x1b26870be06782f112df0b4a6468b85a13628c86",
       error_file: "logs/err.log",
       out_file: "logs/out.log",
     },
     {
       name: "app5",
-      script: "node listen 0x0b01f1310e7224dafed24c3b62d53cec37d9faf8",
+      script: "node listen 0x23606d3a1c297f8bbdea0f4a9b3285e35f3a9ecf",
       error_file: "logs/err.log",
       out_file: "logs/out.log",
     },
     {
       name: "app6",
-      script: "node listen 0xf9f0357268b76661fdaa433c96fffe4311daa15f",
+      script: "node listen 0x0b01f1310e7224dafed24c3b62d53cec37d9faf8",
       error_file: "logs/err.log",
       out_file: "logs/out.log",
     },
     {
       name: "app7",
-      script: "node listen 0xddd3227cc48d04e5ea17e8b9def870c45160501e",
-      error_file: "logs/err.log",
-      out_file: "logs/out.log",
-    },{
-      name: "app8",
-      script: "node listen 0x0B01F1310e7224DAfEd24C3B62d53CeC37d9fAf8",
+      script: "node listen 0x12cc380c848c682248cc1336052b1a72f022ecf0",
       error_file: "logs/err.log",
       out_file: "logs/out.log",
     },
-    // {
-    //   name: "test",
-    //   script:
-    //     "node test/listen_rinkeby 0xF1291E8B0a81f4E32C7D93bb91793b754524dBe2 --alarm",
-    //   error_file: "logs/err.log",
-    // },
-    // {
-    //   name: 'test',
-    //   script: 'node test/try_alarm'
-    // },
-    // {
-    //   name: 'test1',
-    //   script: 'node test/test_email'
-    // }
+    {
+      name: "app8",
+      script: "node listen 0x9308c261dc19178a8b81c07b80caade0ff057f6e",
+      error_file: "logs/err.log",
+      out_file: "logs/out.log",
+    },
+    {
+      name: "app9",
+      script: "node listen 0x405ce2d22faff3a0420568c84cc660548070ce52",
+      error_file: "logs/err.log",
+      out_file: "logs/out.log",
+    },
+    {
+      name: "app9",
+      script: "node listen 0xd045cc3e4428222eeb1602d63897a32c32440a6c",
+      error_file: "logs/err.log",
+      out_file: "logs/out.log",
+    },
+    {
+      name: "app9",
+      script: "node listen 0xa5f6d896e8b4d29ac6e5d8c4b26f8d2073ac90ae",
+      error_file: "logs/err.log",
+      out_file: "logs/out.log",
+    },
   ],
 }
